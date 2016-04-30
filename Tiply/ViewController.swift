1bac5d8c397fb34c6b7443a788a5dfee4f442af3
vik denic <vik.denic@gmail.com>
2016-04-30 17:07:39 -0500

    Initial Commit

---
+++ Tiply/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tiply
+//
+//  Created by Vik Denic on 4/30/16.
+//  Copyright © 2016 vikzilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
