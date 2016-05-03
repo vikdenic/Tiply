5940c12a854341437d9eda24bb77df09acb61b53
vik denic <vik.denic@gmail.com>
2016-05-02 20:16:26 -0500

    control split count

--- Tiply/ViewController.swift
+++ Tiply/ViewController.swift
@@ -11,10 +11,29 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBOutlet var gratStackView: UIStackView!
+  @IBOutlet var billTextField: UITextField!
+  @IBOutlet var tipLabel: UILabel!
+  @IBOutlet var totalLabel: UILabel!
+  @IBOutlet var splitLabel: UILabel!
 
+  var splitCount = 1 {
+    didSet {
+      splitLabel.text = "\(splitCount)"
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+
+  @IBAction func onArrowTapped(sender: UIButton) {
+    if splitCount == 1 {
+      if sender.tag == 1 {
+        splitCount++
+      }
+    } else {
+      sender.tag == 0 ? splitCount-- : splitCount++
+    }
   }
 
 
