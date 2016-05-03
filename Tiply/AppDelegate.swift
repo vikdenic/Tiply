30c9ed9e6815619ef2eed7a28dfd0a881beb8a3b
vik denic <vik.denic@gmail.com>
2016-05-03 01:55:37 -0500

    new fonts and white status bar

--- Tiply/AppDelegate.swift
+++ Tiply/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        return true
+      application.statusBarStyle = .LightContent
+      return true
     }
 
     func applicationWillResignActive(application: UIApplication) {
