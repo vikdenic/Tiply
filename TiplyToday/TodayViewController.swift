276aed918931835430c122809cfdafdd2180ccf4
vik denic <vik.denic@gmail.com>
2016-05-04 00:39:06 -0500

    show percent button selection

--- TiplyToday/TodayViewController.swift
+++ TiplyToday/TodayViewController.swift
@@ -10,25 +10,30 @@ import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
 
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
+  @IBOutlet var percentButtons: [UIButton]!
+  @IBOutlet var percent15Button: UIButton!
 
-        completionHandler(NCUpdateResult.NewData)
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    percent15Button.selected = true
+  }
+
+  @IBAction func onPercentButtonTapped(sender: UIButton) {
+    for button in percentButtons {
+      button.selected = false
     }
+    sender.selected = true
+  }
+    
+  func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
+    // Perform any setup necessary in order to update the view.
+
+    // If an error is encountered, use NCUpdateResult.Failed
+    // If there's no update required, use NCUpdateResult.NoData
+    // If there's an update, use NCUpdateResult.NewData
+
+    completionHandler(NCUpdateResult.NewData)
+  }
     
 }
